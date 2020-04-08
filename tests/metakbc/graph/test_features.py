772bacd6e33bdfcf0da672a1c1b04e78f42a8605
Pasquale Minervini <p.minervini@gmail.com>
2020-04-08 01:32:11 +0100

    update

--- tests/metakbc/graph/test_features.py
+++ tests/metakbc/graph/test_features.py
@@ -19,7 +19,9 @@ def test_features_v1():
     for root, dirs, files in os.walk('data/'):
         for file in files:
             if file.endswith('.tsv'):
-                data = Data(train_path='data/wn18rr/train.tsv')
+                path = f'{root}/{file}'
+                # print(path)
+                data = Data(train_path=path)
                 features = get_features(data.train_triples, data.entity_to_idx, data.predicate_to_idx)
                 if nb_features is None:
                     nb_features = features.shape[1]
@@ -27,6 +29,6 @@ def test_features_v1():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     pytest.main([__file__])
     # test_features_v1()
