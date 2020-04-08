a3833fbe7975edb81c5a6c08db3fff57c1297964
Pasquale Minervini <p.minervini@gmail.com>
2020-04-08 17:58:33 +0100

    update

--- tests/metakbc/regularizers/test_regularizers.py
+++ tests/metakbc/regularizers/test_regularizers.py
@@ -5,24 +5,20 @@ import numpy as np
 import torch
 from torch import nn
 
-from metakbc.regularizers import N3
+from metakbc.regularizers import L1, F2, N3
 
 import pytest
 
 
 @pytest.mark.light
 def test_regularizers_v1():
-    regularizer = N3()
-
-    T = torch.rand(32, 64)
-
-    print(T.shape)
-
-    R = regularizer([T])
-
-    print(R.shape)
+    regularizer_lst = [L1(), F2(), N3()]
+    for regularizer in regularizer_lst:
+        T = torch.rand(32, 64)
+        assert [x for x in regularizer([T]).shape] == []
+        assert [x for x in regularizer([T], dim=1).shape] == [32]
 
 
 if __name__ == '__main__':
     pytest.main([__file__])
-    # test_regularizers_v1()
+
