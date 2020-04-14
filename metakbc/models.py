95bcf61e01566ee3b9746ddc20f45ab33890d1c1
SimonNick <github@lehs.de>
2020-04-14 16:32:23 +0200

    added ComplEx

--- metakbc/models.py
+++ metakbc/models.py
@@ -68,7 +68,6 @@ class DistMult(BaseModel):
         p, o = self.emb_p[p_idx], self.emb_so[o_idx]
         return (p * o) @ self.emb_so.t()
 
-
     def score_objects(self,
                        s_idx: LongTensor,
                        p_idx: LongTensor) -> Tensor:
@@ -76,87 +75,84 @@ class DistMult(BaseModel):
         s, p = self.emb_so[s_idx], self.emb_p[p_idx]
         return (p * s) @ self.emb_so.t()
 
-
     def forward(self, *args):
         return self.score(*args)
 
 
 
+class ComplEx(BaseModel):
 
-# class ComplEx(BaseModel):
-#     def __init__(self) -> None:
-#         super().__init__()
+    def __init__(self, size: Tuple[int, int, int], rank: int) -> None:
+        super().__init__()
 
-#     def score(self,
-#               s: Tensor,
-#               p: Tensor,
-#               o: Tensor) -> Tensor:
+        self.emb_so = Parameter(torch.empty((size[0], 2*rank)).normal_())
+        self.emb_p = Parameter(torch.empty((size[1], 2*rank)).normal_())
 
-#         rank = p.shape[1] // 2
+    def _scoring_func(self,
+                      s: Tensor,
+                      p: Tensor,
+                      o: Tensor) -> Tensor:
+        raise NotImplementedError
 
-#         # [B, E]
-#         p_real, p_img = p[:, :rank], p[:, rank:]
-#         s_real, s_img = s[:, :rank], s[:, rank:]
-#         o_real, o_img = o[:, :rank], o[:, rank:]
+    def score(self,
+              s_idx: LongTensor,
+              p_idx: LongTensor,
+              o_idx: LongTensor) -> Tensor:
 
-#         # [B] Tensor
-#         score1 = torch.sum(p_real * s_real * o_real, 1)
-#         score2 = torch.sum(p_real * s_img * o_img, 1)
-#         score3 = torch.sum(p_img * s_real * o_img, 1)
-#         score4 = torch.sum(p_img * s_img * o_real, 1)
+        s, p, o = self.emb_so[s_idx], self.emb_p[p_idx], self.emb_so[o_idx]
 
-#         res = score1 + score2 + score3 - score4
+        rank = p.shape[1] // 2
 
-#         # [B] Tensor
-#         return res
+        p_real, p_img = p[:, :rank], p[:, rank:]
+        s_real, s_img = s[:, :rank], s[:, rank:]
+        o_real, o_img = o[:, :rank], o[:, rank:]
 
-#     def forward(self,
-#                 s: Optional[Tensor],
-#                 p: Tensor,
-#                 o: Optional[Tensor],
-#                 entity_embeddings: Tensor) -> Tuple[Optional[Tensor], Optional[Tensor]]:
+        score1 = torch.sum(p_real * s_real * o_real, 1)
+        score2 = torch.sum(p_real * s_img * o_img, 1)
+        score3 = torch.sum(p_img * s_real * o_img, 1)
+        score4 = torch.sum(p_img * s_img * o_real, 1)
 
-#         rank = p.shape[1] // 2
-#         emb = entity_embeddings
+        res = score1 + score2 + score3 - score4
 
-#         p_real, p_img = p[:, :rank], p[:, rank:]
-#         emb_real, emb_img = emb[:, :rank], emb[:, rank:]
+        return res
 
-#         # [B] Tensor
-#         score_sp = score_po = None
+    def score_subjects(self,
+                       p_idx: LongTensor,
+                       o_idx: LongTensor) -> Tensor:
 
-#         if s is not None:
-#             s_real, s_img = s[:, :rank], s[:, rank:]
+        p, o = self.emb_p[p_idx], self.emb_so[o_idx]
+        rank = p.shape[1] // 2
+        p_real, p_img = p[:, :rank], p[:, rank:]
+        o_real, o_img = o[:, :rank], o[:, rank:]
+        emb_real, emb_img = self.emb_so[:, :rank], self.emb_so[:, rank:]
 
-#             score1_sp = (p_real * s_real) @ emb_real.t()
-#             score2_sp = (p_real * s_img) @ emb_img.t()
-#             score3_sp = (p_img * s_real) @ emb_img.t()
-#             score4_sp = (p_img * s_img) @ emb_real.t()
+        score1_po = (p_real * o_real) @ emb_real.t()
+        score2_po = (p_real * o_img) @ emb_img.t()
+        score3_po = (p_img * o_img) @ emb_real.t()
+        score4_po = (p_img * o_real) @ emb_img.t()
 
-#             score_sp = score1_sp + score2_sp + score3_sp - score4_sp
+        score_po = score1_po + score2_po + score3_po - score4_po
 
-#         if o is not None:
-#             o_real, o_img = o[:, :rank], o[:, rank:]
+        return score_po
 
-#             score1_po = (p_real * o_real) @ emb_real.t()
-#             score2_po = (p_real * o_img) @ emb_img.t()
-#             score3_po = (p_img * o_img) @ emb_real.t()
-#             score4_po = (p_img * o_real) @ emb_img.t()
+    def score_objects(self,
+                       s_idx: LongTensor,
+                       p_idx: LongTensor) -> Tensor:
 
-#             score_po = score1_po + score2_po + score3_po - score4_po
+        s, p = self.emb_so[s_idx], self.emb_p[p_idx]
+        rank = p.shape[1] // 2
+        s_real, s_img = s[:, :rank], s[:, rank:]
+        p_real, p_img = p[:, :rank], p[:, rank:]
+        emb_real, emb_img = self.emb_so[:, :rank], self.emb_so[:, rank:]
 
-#         return score_sp, score_po
+        score1_sp = (p_real * s_real) @ emb_real.t()
+        score2_sp = (p_real * s_img) @ emb_img.t()
+        score3_sp = (p_img * s_real) @ emb_img.t()
+        score4_sp = (p_img * s_img) @ emb_real.t()
 
-# #     def factor(self,
-# #                embedding_vector: Tensor,
-# #                safe: bool = False) -> Tensor:
-# #         rank = embedding_vector.shape[1] // 2
+        score_sp = score1_sp + score2_sp + score3_sp - score4_sp
 
-# #         vec_real = embedding_vector[:, :rank]
-# #         vec_img = embedding_vector[:, rank:]
+        return score_sp
 
-# #         sq_factor = vec_real ** 2 + vec_img ** 2
-# #         if safe is True:
-# #             sq_factor = torch.max(sq_factor, self.eps)
-
-# #         return torch.sqrt(sq_factor)
+    def forward(self, *args):
+        return self.score(*args)
