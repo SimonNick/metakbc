e036df9cbe23b2675adc3f6038d8c820bb7bad35
SimonNick <github@lehs.de>
2020-04-22 22:22:57 +0200

    added filtered ranking

--- metakbc/evaluation.py
+++ metakbc/evaluation.py
@@ -1,7 +1,8 @@
 import torch
 from torch import Tensor, LongTensor
 
-from typing import Tuple, List
+from typing import Tuple, List, Optional
+from collections import defaultdict
 
 from metakbc.datasets import Dataset
 
@@ -9,8 +10,9 @@ from metakbc.datasets import Dataset
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def metrics(scores: Tensor, true_idx: LongTensor) -> dict:
+def metrics(scores: Tensor, true_idx: LongTensor, masks: Optional[Tensor] = None) -> dict:
 
+    if not masks == None: scores[masks] = -float('inf')
     argsort_score = torch.argsort(scores, dim=1, descending=True)
     onehot_match = (argsort_score == true_idx.view(-1, 1)).float()
     
@@ -36,7 +38,8 @@ def add_averages(average_a: float, size_a: int, average_b: float, size_b: int) -
 def evaluate(dataset: Dataset,
              splits: List[str],
              model,
-             batch_size: int) -> dict:
+             batch_size: int,
+             filters: Optional[dict] = None) -> dict:
 
     metrics_dict = {s: {m: 0 for m in ["MRR", "HITS@3", "HITS@5", "HITS@10"]} for s in splits}
 
@@ -46,6 +49,7 @@ def evaluate(dataset: Dataset,
         for batch in dataset.get_batches(s, batch_size, shuffle=False):
         
             batch = batch.to(device)
+            n_batch_samples = batch.shape[0]
             s_idx = batch[:, 0]
             p_idx = batch[:, 1]
             o_idx = batch[:, 2]
@@ -56,10 +60,33 @@ def evaluate(dataset: Dataset,
             scores = torch.cat((score_s, score_o))
             idx = torch.cat((s_idx, o_idx))
 
-            batch_metrics = metrics(scores, idx)
-            n_batch_samples = batch.shape[0]
+            if not filters == None:
+                s_masks = torch.zeros(n_batch_samples, dataset.n_entities, dtype=torch.bool)
+                o_masks = torch.zeros(n_batch_samples, dataset.n_entities, dtype=torch.bool)
+                for i in range(n_batch_samples):
+                    for hide_s in filters['s'][(p_idx[i].item(), o_idx[i].item())]:
+                        s_masks[i,hide_s] = True
+                    for hide_o in filters['o'][(s_idx[i].item(), p_idx[i].item())]:
+                        o_masks[i,hide_o] = True
+                    s_masks[i,s_idx[i].item()] = False
+                    o_masks[i,o_idx[i].item()] = False
+                masks = torch.cat((s_masks, o_masks))
+            else:
+                masks = None
+
+            batch_metrics = metrics(scores, idx, masks)
             for k,v in metrics_dict[s].items():
                 metrics_dict[s][k] = add_averages(v, n_samples, batch_metrics[k], n_batch_samples)
             n_samples += n_batch_samples
 
     return metrics_dict
+
+
+def build_filters(dataset: Dataset) -> dict:
+    filters = {'s': defaultdict(set), 'o': defaultdict(set)}
+    for split in dataset.splits:
+        for e in dataset.get_examples(split):
+            s, p, o = e[0].item(), e[1].item(), e[2].item()
+            filters['s'][(p,o)].add(s)
+            filters['o'][(s,p)].add(o)
+    return filters
