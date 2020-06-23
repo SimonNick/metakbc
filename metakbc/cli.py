61d9d4cdeee12c2e499cf359c5f45efe70eda91c
SimonNick <github@lehs.de>
2020-06-23 21:49:32 +0200

    fix

--- metakbc/cli.py
+++ metakbc/cli.py
@@ -5,8 +5,9 @@ import argparse
 from metakbc.learn import learn
 
 import wandb
+import datetime
 
-datasets = ['Toy', 'Toy2', 'Toy2_2', 'Toy2_3', 'nations', 'umls', 'Toy_A,B=>C']
+datasets = ['Toy_A,B=>C_16', 'Toy_A=>B_10', 'Toy_A=>B_1024', 'Toy_A=>B,C=>D_5000', 'nations', 'umls', 'countries']
 models = ['DistMult', 'ComplEx']
 optimizers = ['SGD', 'Adagrad']
 methods = ['offline', 'online']
@@ -28,14 +29,14 @@ if __name__ == '__main__':
 
     parser.add_argument('--lr',             default=0.1,        type=float,             help="Learning rate of the optimizer")
     parser.add_argument('--meta_lr',        default=0.1,        type=float,             help="Learning rate of the meta optimizer")
-    parser.add_argument('--adv_lr',         default=0.5,        type=float,             help="Learning rate of the adversary optimizer")
+    parser.add_argument('--adv_lr',         default=0.05,       type=float,             help="Learning rate of the adversary optimizer")
 
     parser.add_argument('--epochs_outer',   default=100,        type=int,               help="Number of outer epochs")
     parser.add_argument('--epochs_inner',   default=100,        type=int,               help="Number of inner epochs for offline metalearning")
     parser.add_argument('--batches_train',  default=5,          type=int,               help="How many batches of the training dataset should be used for training for online metalearning")
     parser.add_argument('--batches_valid',  default=5,          type=int,               help="How many batches of the validation dataset should be used for evaluation for online metalearning")
     parser.add_argument('--epochs_adv',     default=100,        type=int,               help="Number of epochs for the adversary")
-    parser.add_argument('--valid',          default=10,         type=int,               help="Number of skipped epochs until evaluation")
+    parser.add_argument('--valid',          default=5,          type=int,               help="Number of skipped epochs until evaluation")
 
     parser.add_argument('--rank',           default=100,        type=int,               help="Rank of the tensor decomposition")
     parser.add_argument('--batch_size',     default=32,         type=int,               help="Batch size for training and evaluation")
@@ -46,7 +47,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.logging:
-        wandb.init(project="test1")
+        name = datetime.datetime.now().strftime("%A %d.%m. %H:%M")
+        wandb.init(project="meta-kbc", name=name)
 
         wandb.config.dataset = args.dataset
         wandb.config.model = args.model
@@ -56,14 +58,14 @@ if __name__ == '__main__':
 
         wandb.config.optimizer = args.optimizer
         wandb.config.meta_optimizer = args.meta_optimizer
-        wandb.config.adv_optimizer = args.meta_optimizer
+        wandb.config.adv_optimizer = args.adv_optimizer
 
         wandb.config.lr = args.lr
         wandb.config.meta_lr = args.meta_lr
-        wandb.config.adv_lr = args.meta_lr
+        wandb.config.adv_lr = args.adv_lr
 
         wandb.config.n_epochs_outer = args.epochs_outer
-        if method == 'offline':
+        if args.method == 'offline':
             wandb.config.n_epochs_inner = args.epochs_inner
         else:
             wandb.config.batches_train = args.batches_train
