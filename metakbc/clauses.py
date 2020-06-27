1e85d8df83e7c0de11ec3707a538326211f4cbfc
SimonNick <github@lehs.de>
2020-06-27 18:33:24 +0200

    use 10 constraints

--- metakbc/clauses.py
+++ metakbc/clauses.py
@@ -19,7 +19,7 @@ def load_clauses(dataset: Dataset) -> List[LearnedClause]:
 
         # A(X1, X2), B(X2, X3) => C(X1, X3)
         clause_func = lambda x1, x2, x3, phi1, phi2, phi3: torch.min(phi1(x1, x2), phi2(x2, x3)) - phi3(x1, x3)
-        clause = LearnedClause(n_variables=3, n_relations=3, clause_loss_func=clause_func, n_constraints=2, n_predicates=dataset.n_predicates)
+        clause = LearnedClause(n_variables=3, n_relations=3, clause_loss_func=clause_func, n_constraints=10, n_predicates=dataset.n_predicates)
 
         return [clause]
 
