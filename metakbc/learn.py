61c44690bf91bf82eef795c44e0ad8798533c2d2
SimonNick <github@lehs.de>
2020-07-02 19:25:02 +0200

    gradient clipping

--- metakbc/learn.py
+++ metakbc/learn.py
@@ -99,7 +99,7 @@ def learn(dataset_str: str,
 
         meta_optim.zero_grad() 
         loss_valid.backward() 
-        clip_grad_value_(adversary.parameters(), 1e-2)
+        clip_grad_value_(adversary.parameters(), 1e-1)
         meta_optim.step()
 
         if learn_lam:
