caf65e7ff006e71fc43456b838d22d6bf978bc96
SimonNick <github@lehs.de>
2020-08-11 01:35:04 +0100

    fix

--- metakbc/clauses.py
+++ metakbc/clauses.py
@@ -7,7 +7,7 @@ from typing import List
 
 def load_clauses(dataset: Dataset, method: str) -> List[LearnedClause]:
 
-    if dataset.name in ["Toy_A=>B_10", "Toy_A=>B_1024"]:
+    if dataset.name in ["Toy_A=>B_16", "Toy_A=>B_1024"]:
 
         # A(X1, X2) => B(X1, X2)
         clause_func = lambda x1, x2, phi1, phi2: phi1(x1, x2) - phi2(x1, x2)
@@ -15,7 +15,7 @@ def load_clauses(dataset: Dataset, method: str) -> List[LearnedClause]:
 
         return [clause]
 
-    if dataset.name in ["Toy_A=>B,C=>D_5000", "Toy_A=>B,C=>D_1024"]:
+    if dataset.name in ["Toy_A=>B,C=>D_1024"]:
 
         # A(X1, X2) => B(X1, X2)
         clause_func = lambda x1, x2, phi1, phi2: phi1(x1, x2) - phi2(x1, x2)
@@ -31,7 +31,7 @@ def load_clauses(dataset: Dataset, method: str) -> List[LearnedClause]:
 
         return [clause]
 
-    if dataset.name in ["Toy_A,B=>C,D,E=>F_1024", "Toy_A,B=>C,D,E=>F_32"]:
+    if dataset.name in ["Toy_A,B=>C,D,E=>F_1024"]:
 
         # A(X1, X2), B(X2, X3) => C(X1, X3)
         clause_func = lambda x1, x2, x3, phi1, phi2, phi3: torch.min(phi1(x1, x2), phi2(x2, x3)) - phi3(x1, x3)
@@ -54,14 +54,6 @@ def load_clauses(dataset: Dataset, method: str) -> List[LearnedClause]:
         clause3 = LearnedClause(n_variables=3, n_relations=3, clause_loss_func=clause_func3, n_constraints=200, n_predicates=dataset.n_predicates, method=method)
 
         return [clause1, clause2, clause3]
-
-    if dataset.name in ["Toy_A=>A_10", "Toy_A=>A_1024"]:
-
-        # A(X1, X2) => A(X2, X1)
-        clause_func = lambda x1, x2, phi1: phi1(x1, x2) - phi1(x2, x1)
-        clause = LearnedClause(n_variables=2, n_relations=1, clause_loss_func=clause_func, n_constraints=1, n_predicates=dataset.n_predicates, method=method)
-
-        return [clause]
 
     if dataset.name == "countries":
 
